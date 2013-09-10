19297d31b76107222801e7882379d38d82bd94b0
Ondřej Čertík <ondrej.certik@gmail.com>
2013-09-09 21:12:42 -0600

    Call fftpack

--- test.f90
+++ test.f90
@@ -1,3 +1,63 @@
 program test
+integer, parameter :: dp=kind(0.d0)
+real(dp) :: t1, t2
+real(dp), allocatable :: x(:), wsave(:)
+complex(dp), allocatable :: xdft(:)
 print *, "test"
+call init_random()
+n = 1024 * 1024
+allocate(x(n), xdft(n), wsave(n+int(log(real(n, dp))/log(2._dp)) + 4))
+call random_number(x)
+call cpu_time(t1)
+call cffti1(n, wsave, size(wsave), ier)
+call assert(ier == 0)
+call cpu_time(t2)
+print *, "time:", (t2-t1)*1000, "ms"
+
+contains
+
+subroutine init_random()
+! Initializes the random number generator based on the system's time.
+integer :: i, n, clock
+integer, allocatable :: seed(:)
+call random_seed(size=n)
+allocate(seed(n))
+call system_clock(count=clock)
+seed = clock + 37 * [(i - 1, i = 1, n)]
+call random_seed(put=seed)
+end subroutine
+
+subroutine assert(condition)
+! If condition == .false., it aborts the program.
+!
+! Arguments
+! ---------
+!
+logical, intent(in) :: condition
+!
+! Example
+! -------
+!
+! call assert(a == 5)
+
+if (.not. condition) call stop_error("Assert failed.")
+end subroutine
+
+subroutine stop_error(msg)
+! Aborts the program with nonzero exit code
+!
+! The statement "stop msg" will return 0 exit code when compiled using
+! gfortran. stop_error() uses the statement "stop 1" which returns an exit code
+! 1 and a print statement to print the message.
+!
+! Example
+! -------
+!
+! call stop_error("Invalid argument")
+
+character(len=*) :: msg ! Message to print on stdout
+print *, msg
+stop 1
+end subroutine
+
 end program
