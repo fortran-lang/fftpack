887aee05a875f1441cf0f3f7e6a565a7866da54b
Ondřej Čertík <ondrej.certik@gmail.com>
2013-09-11 12:55:46 -0600

    Use factor 2 only

--- cffti1.f
+++ cffti1.f
@@ -25,7 +25,9 @@
       IFAC(3) = 2
   107 IF (NL .NE. 1) GO TO 104
       IFAC(1) = N
+      NF = 5
       IFAC(2) = NF
+      IFAC(3:3+NF) = 2
       TPI =  6.28318530717958647692D0
       ARGH = TPI/FLOAT(N)
       I = 2
