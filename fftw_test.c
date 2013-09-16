f1b05291f41110615f20790fee4892db24f72152
Ondřej Čertík <ondrej.certik@gmail.com>
2013-09-16 14:32:56 -0600

    Use complex number, initialize the array

--- fftw_test.c
+++ fftw_test.c
@@ -1,9 +1,11 @@
+#include <complex.h>
+#include <stdlib.h>
 #include <time.h>
 #include <fftw3.h>
 int main()
 {
-    clock_t t1, t2, t3;
-    int N;
+    clock_t t1, t2, t3, t4;
+    int N, i;
     N = 1024*1024;
     fftw_complex *in, *out;
     fftw_plan p;
@@ -15,12 +17,15 @@ int main()
     //p = fftw_plan_dft_1d(N, in, out, FFTW_FORWARD, FFTW_MEASURE);
     t2 = clock();
 
-    fftw_execute(p); /* repeat as needed */
-    t3 = clock();
+    for (i=0; i < N; i++) in[i] = (double) rand() / ((double)RAND_MAX + 1);
 
-    printf("Total time: %f ms\n", 1000. * (t3-t1) / CLOCKS_PER_SEC);
+    t3 = clock();
+    fftw_execute(p); /* repeat as needed */
+    t4 = clock();
+
+    printf("Total time: %f ms\n", 1000. * (t4-t3 + t2-t1) / CLOCKS_PER_SEC);
     printf("Init time : %f ms\n", 1000. * (t2-t1) / CLOCKS_PER_SEC);
-    printf("Calc time : %f ms\n", 1000. * (t3-t2) / CLOCKS_PER_SEC);
+    printf("Calc time : %f ms\n", 1000. * (t4-t3) / CLOCKS_PER_SEC);
 
     fftw_destroy_plan(p);
     fftw_free(in); fftw_free(out);
