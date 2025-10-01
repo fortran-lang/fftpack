f36ef88e45ad49e656a79df7adc41e620746d80c
Jean-Christophe <loiseau.jc@gmail.com>
2025-10-01 21:30:26 +0200

    Update src/dfftb.f90
    Co-authored-by: ZUO Zhihua <zuo.zhihua@qq.com>

--- src/dfftb.f90
+++ src/dfftb.f90
@@ -3,7 +3,7 @@
          implicit none
          integer, intent(in) :: n
          real(dp), intent(inout) :: r(*)
-         real(dp), intent(in) :: wsave(*)
+         real(dp), intent(inout) :: wsave(*)
          if (n == 1) return
          call rfftb1(n, r, wsave, wsave(n + 1), wsave(2*n + 1))
       end subroutine dfftb
